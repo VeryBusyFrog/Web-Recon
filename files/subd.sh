d5cb405dbb6b2d7a1d4a3de68e59e06ed598710b
VeryBusyFrog <qstormheaven@gmail.com>
2024-06-11 21:33:51 +0300

    Update subd.sh

--- files/subd.sh
+++ files/subd.sh
@@ -2,4 +2,4 @@
 read -p 'Enter url: ' url
 read -p 'Enter host: ' host
 
-ffuf -u $url -H 'Host: FUZZ.$host' -w /home/tools/wordlists/subdomains-bitquark-top100000.txt -t 128 -fs 12330 -o /home/tools/outputs -of html
+ffuf -w /home/tools/wordlists/subdomains-bitquark-top100000.txt -u $url -H 'Host: FUZZ.$host'  -t 128 -fs 12330 -o /home/tools/outputs -of html
